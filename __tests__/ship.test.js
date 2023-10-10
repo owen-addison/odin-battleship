b772ce777885f4a5a781a48ddf0b39d30218ad29
Owen Lewis <owenalewis@gmail.com>
2023-10-10 20:08:47 +0100

    Adds Ship factory & exports, imports to test file

---
+++ __tests__/ship.test.js
@@ -0,0 +1,11 @@
+import Ship from "../src/ship";
+
+// Test for ship length
+
+// Test for initial hit count
+
+// Test for hit positions
+
+// Test for isSunk with partial hits
+
+// Test for isSunk with full hits
