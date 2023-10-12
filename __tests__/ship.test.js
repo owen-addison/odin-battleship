cdd28a62e345087291631922dd0f1ff1ccc845f6
Owen Lewis <owenalewis@gmail.com>
2023-10-12 16:49:28 +0100

    Writes test for hit count not exceeding ship length

--- __tests__/ship.test.js
+++ __tests__/ship.test.js
@@ -14,6 +14,18 @@ test("Newly created ship to have zero hits", () => {
   expect(ship.hits).toBe(0);
 });
 
+// Test for hit count not exceeding ship length
+test("Number of hits does not exceed ship's length", () => {
+  const ship = Ship(2);
+
+  for (let i = 0; i < ship.length + 2; i++) {
+    ship.hit();
+    console.log(`hit number ${i + 1}`);
+  }
+
+  expect(ship.hits).toBe(ship.length);
+});
+
 // Test for hit positions
 
 // Test for isSunk with partial hits
