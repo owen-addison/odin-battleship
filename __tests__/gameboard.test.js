40723cbbda1c360191c8a5057fb3b71abda5aa38
Owen Lewis <owenalewis@gmail.com>
2023-10-18 18:32:21 +0100

    Writes test checking init gameboard has correct dims

--- __tests__/gameboard.test.js
+++ __tests__/gameboard.test.js
@@ -1,1 +1,14 @@
 import Gameboard from "../src/gameboard";
+
+// Test if the game board is initialised with the correct dimensions
+test("Gameboard to be created with correct dimensions of 4x4", () => {
+  const newGame = Gameboard();
+
+  // Check the number of rows
+  expect(newGame.grid).toHaveLength(4);
+
+  // Check the number of columns for each row
+  newGame.gird.forEach((row) => {
+    expect(row).toHaveLength(4);
+  });
+});
