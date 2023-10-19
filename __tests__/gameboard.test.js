79beb4bd37d0675d4ea7641064775e01e21ec69f
Owen Lewis <owenalewis@gmail.com>
2023-10-19 14:23:21 +0100

    Adds structure to file & corrects typo Adds structure to tests using describe blocks. Adds describe blocks for the other test categories. Corrects typo of the word "grid".

--- __tests__/gameboard.test.js
+++ __tests__/gameboard.test.js
@@ -1,22 +1,36 @@
 import Gameboard from "../src/gameboard";
 
-// Test if the game board is initialised with the correct dimensions
-test("Gameboard to be created with correct dimensions of 10x10", () => {
-  const newGame = Gameboard();
+describe("Gameboard Initialisation", () => {
+  // Test if the game board is initialised with the correct dimensions
+  test("Gameboard to be created with correct dimensions of 10x10", () => {
+    const newGame = Gameboard();
 
-  // Check the number of rows
-  expect(newGame.grid).toHaveLength(10);
+    // Check the number of rows
+    expect(newGame.grid).toHaveLength(10);
 
-  // Check the number of columns for each row
-  newGame.gird.forEach((row) => {
-    expect(row).toHaveLength(10);
+    // Check the number of columns for each row
+    newGame.grid.forEach((row) => {
+      expect(row).toHaveLength(10);
+    });
+  });
+
+  // Test if the game board is empty (no ships) on initialisation
+  test("Gameboard to be created empty without any ships", () => {
+    const emptyBoard = Gameboard();
+
+    // Check if the ships array is empty
+    expect(emptyBoard.ships).toHaveLength(0);
   });
 });
 
-// Test if the game board is empty (no ships) on initialisation
-test("Gameboard to be created empty without any ships", () => {
-  const emptyBoard = Gameboard();
+describe("Basic Board & Ship Interactions", () => {});
 
-  // Check if the ships array is empty
-  expect(emptyBoard.ships).toHaveLength(0);
-});
+describe("Boundary & Error Cases for Ship Placement", () => {});
+
+describe("Valid Ship Set Functionality", () => {});
+
+describe("Basic Attack Mechanisms", () => {});
+
+describe("Complex Attack Scenarios", () => {});
+
+describe("Game State & Reporting", () => {});
