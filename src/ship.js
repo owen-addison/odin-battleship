49ee9929480575a838b8841c2979d64406623612
Owen Lewis <owenalewis@gmail.com>
2023-11-14 11:32:12 +0100

    Refactor Ship Factory for Position Handling Commented out position calculation in Ship factory in preparation for Gameboard to manage ship placements.

--- src/ship.js
+++ src/ship.js
@@ -1,4 +1,4 @@
-import { grid } from "./gameboard";
+// import { grid } from "./gameboard";
 
 const Ship = (shipLength = 2, anchorPoint = "A1", orientation = "h") => {
   let hits = 0;
@@ -6,20 +6,20 @@ const Ship = (shipLength = 2, anchorPoint = "A1", orientation = "h") => {
   const rowLetter = anchorPoint[0].toUpperCase();
   const colNumber = parseInt(anchorPoint[1], 10);
 
-  const rowIndex = rowLetter.charCodeAt(0) - "A".charCodeAt(0);
-  const colIndex = colNumber - 1;
+  // const rowIndex = rowLetter.charCodeAt(0) - "A".charCodeAt(0);
+  // const colIndex = colNumber - 1;
 
-  const shipPositions = [];
+  // const shipPositions = [];
 
-  if (orientation.toLowerCase() === "h") {
-    for (let i = 0; i < shipLength; i++) {
-      shipPositions.push(grid[rowIndex][colIndex + i]);
-    }
-  } else {
-    for (let i = 0; i < shipLength; i++) {
-      shipPositions.push(grid[rowIndex + i][colIndex]);
-    }
-  }
+  // if (orientation.toLowerCase() === "h") {
+  //   for (let i = 0; i < shipLength; i++) {
+  //     shipPositions.push(grid[rowIndex][colIndex + i]);
+  //   }
+  // } else {
+  //   for (let i = 0; i < shipLength; i++) {
+  //     shipPositions.push(grid[rowIndex + i][colIndex]);
+  //   }
+  // }
 
   const hit = () => {
     if (hits < shipLength) {
@@ -36,9 +36,9 @@ const Ship = (shipLength = 2, anchorPoint = "A1", orientation = "h") => {
     get hits() {
       return hits;
     },
-    get positions() {
-      return shipPositions;
-    },
+    // get positions() {
+    //   return shipPositions;
+    // },
     hit,
     isSunk,
   };
