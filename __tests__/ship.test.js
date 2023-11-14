2f5e958cac6cfe98cf70d6f4886a9e76aedf583c
Owen Lewis <owenalewis@gmail.com>
2023-11-14 14:12:44 +0100

    Changes how ship factory is implemented in test To properly reflect the correct implementation of the Ship factory, the tests are updated to take the correct and test for valid type entries. Imports errors from errors.js.

--- __tests__/ship.test.js
+++ __tests__/ship.test.js
@@ -1,22 +1,44 @@
 import Ship from "../src/ship";
+import {
+  OverlappingShipsError,
+  ShipAllocationReachedError,
+  ShipTypeAllocationReachedError,
+  InvalidShipLengthError,
+  InvalidShipTypeError,
+} from "../src/errors";
 
 // Test for ship length
 test("Ship to have correct length", () => {
-  const ship = Ship(3);
+  const ship1 = Ship("carrier");
+  const ship2 = Ship("battleship");
+  const ship3 = Ship("cruiser");
+  const ship4 = Ship("submarine");
+  const ship5 = Ship("destroyer");
 
-  expect(ship.shipLength).toBe(3);
+  expect(ship1.shipLength).toBe(5);
+  expect(ship2.shipLength).toBe(4);
+  expect(ship3.shipLength).toBe(3);
+  expect(ship4.shipLength).toBe(3);
+  expect(ship5.shipLength).toBe(2);
+});
+
+// Test for incorrect ship type and proper error handling
+test("Invalid ship type to throw error", () => {
+  expect(() => {
+    Ship("tugboat");
+  }).toThrow(InvalidShipTypeError);
 });
 
 // Test for initial hit count
 test("Newly created ship to have zero hits", () => {
-  const ship = Ship(2);
+  const ship = Ship("carrier");
 
   expect(ship.hits).toBe(0);
 });
 
 // Test for hit count not exceeding ship length
 test("Number of hits does not exceed ship's length", () => {
-  const ship = Ship(2);
+  const ship = Ship("submarine");
 
   for (let i = 0; i < ship.shipLength + 2; i++) {
     ship.hit();
@@ -35,16 +57,17 @@ test.skip("Specific positions of ship are recorded correctly", () => {
 
 // Test for isSunk with partial hits
 test("Ship with partial hit DOES NOT register as sunk", () => {
-  const ship = Ship(2);
+  const ship = Ship("destroyer");
 
   ship.hit();
 
+  expect(ship.hits).toBe(1);
   expect(ship.isSunk()).toBe(false);
 });
 
 // Test for isSunk with full hits
 test("Ship with full hits DOES register as sunk", () => {
-  const ship = Ship(3);
+  const ship = Ship("cruiser");
 
   for (let i = 0; i < ship.shipLength; i++) {
     ship.hit();
