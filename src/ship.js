532295548ef757ef72072a9a75b94e18e60c0bf3
Owen Lewis <owenalewis@gmail.com>
2023-11-15 13:19:50 +0100

    Removes redundant, commented-out code

--- src/ship.js
+++ src/ship.js
@@ -27,24 +27,6 @@ const Ship = (type) => {
 
   let hits = 0;
 
-  // const rowLetter = anchorPoint[0].toUpperCase();
-  // const colNumber = parseInt(anchorPoint[1], 10);
-
-  // const rowIndex = rowLetter.charCodeAt(0) - "A".charCodeAt(0);
-  // const colIndex = colNumber - 1;
-
-  // const shipPositions = [];
-
-  // if (orientation.toLowerCase() === "h") {
-  //   for (let i = 0; i < shipLength; i++) {
-  //     shipPositions.push(grid[rowIndex][colIndex + i]);
-  //   }
-  // } else {
-  //   for (let i = 0; i < shipLength; i++) {
-  //     shipPositions.push(grid[rowIndex + i][colIndex]);
-  //   }
-  // }
-
   const hit = () => {
     if (hits < shipLength) {
       hits += 1;
@@ -63,9 +45,6 @@ const Ship = (type) => {
     get hits() {
       return hits;
     },
-    // get positions() {
-    //   return shipPositions;
-    // },
     hit,
     isSunk,
   };
