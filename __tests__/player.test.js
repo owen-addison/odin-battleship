d1725f038dec0df87f42c59888c51e1751dcdcea
Owen Lewis <owenalewis@gmail.com>
2023-11-27 11:35:23 +0100

    [test] Creates test for Player's makeMove method Creates a test for checking that Player's makeMove method correctly logs the move in the moveLog array.

--- __tests__/player.test.js
+++ __tests__/player.test.js
@@ -1,5 +1,6 @@
 import Gameboard from "../src/gameboard";
 import Player from "../src/player";
+import Ship from "../src/ship";
 
 describe("Initialisation Tests", () => {
   // Test if the Player factory correctly creates a player with a given type (human or computer)
@@ -18,5 +19,25 @@ describe("Initialisation Tests", () => {
 
     // Assert that the move log is returned empty
     expect(p.moveLog).toHaveLength(0);
+  });
+});
+
+describe("Attack Method Tests", () => {
+  // Test if the attack/move method correctly logs a move for both human and computer players
+  test("The makeMove method correctly logs a move for both human and computer players", () => {
+    // Create a new gameboard
+    const gb = Gameboard(Ship);
+
+    // Create two players, one human and one computer
+    const p1 = Player("human", gb);
+    const p2 = Player("computer", gb);
+
+    // Call the makeMove methods on both players
+    p1.makeMove("A1");
+    p2.makeMove("A2");
+
+    // Assert that the moveLog for both players is correct
+    expect(p1.moveLog).toEqual(["A1"]);
+    expect(p2.moveLog).toEqual(["A2"]);
   });
 });
