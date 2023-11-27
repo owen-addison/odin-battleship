41aca092cdc9604a335747f1b2774b40c9457212
Owen Lewis <owenalewis@gmail.com>
2023-11-27 11:53:01 +0100

    [feature] makeMove prevents repeat moves Feature is added which prevents make move from making repeat moves. The makeMove method checks the "move" argument against the existing contents of moveLog and if it finds a match throws an error. Comments are also added to make the structure of the function more clear.

--- src/player.js
+++ src/player.js
@@ -1,7 +1,15 @@
+import { RepeatAttackedError } from "./errors";
+
 const Player = (type, gameboard) => {
   const moveLog = [];
 
   const makeMove = (move) => {
+    // If the move exists in the moveLog array, throw an error
+    if (moveLog.find((el) => el === move)) {
+      throw new RepeatAttackedError();
+    }
+
+    // Else, call attack method on gameboard and log move in moveLog
     gameboard.attack(move);
     moveLog.push(move);
   };
