c0640f57210a9d3da64cbd86d4dcbb16e45c1f0e
Owen Lewis <owenalewis@gmail.com>
2023-11-27 14:00:09 +0100

    [feature] Adds move formatting and validity check Adds feature for formatting the move coordinates received from a Player, as well as checking whether the move is a valid move or not. The makeMove function now formats lowercase move entries to uppercase and then checks them against the possible moves as per the Gameboard's grid matrix.

--- src/player.js
+++ src/player.js
@@ -1,17 +1,37 @@
-import { RepeatAttackedError } from "./errors";
+import { InvalidMoveEntryError, RepeatAttackedError } from "./errors";
+
+const checkMove = (move, gbGrid) => {
+  let valid = false;
+
+  gbGrid.forEach((el) => {
+    if (el.find((p) => p === move)) {
+      valid = true;
+    }
+  });
+
+  return valid;
+};
 
 const Player = (type, gameboard) => {
   const moveLog = [];
 
   const makeMove = (move) => {
+    // Format the input
+    const fMove = `${move.charAt(0).toUpperCase()}${move.substring(1)}`;
+
+    // Check the input against the possible moves on the gameboard's grid
+    if (!checkMove(fMove, gameboard.grid)) {
+      throw new InvalidMoveEntryError();
+    }
+
     // If the move exists in the moveLog array, throw an error
-    if (moveLog.find((el) => el === move)) {
+    if (moveLog.find((el) => el === fMove)) {
       throw new RepeatAttackedError();
     }
 
     // Else, call attack method on gameboard and log move in moveLog
-    gameboard.attack(move);
-    moveLog.push(move);
+    gameboard.attack(fMove);
+    moveLog.push(fMove);
   };
 
   return {
