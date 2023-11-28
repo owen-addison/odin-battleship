d8b08f3de76cbf42a9928ca58ee133c31b1ce317
Owen Lewis <owenalewis@gmail.com>
2023-11-28 17:42:20 +0100

    [feature] Adds randMove ability to makeMove Adds a randMove function and uses it within makeMove to make a random move if the player type is "computer".

--- src/player.js
+++ src/player.js
@@ -12,26 +12,47 @@ const checkMove = (move, gbGrid) => {
   return valid;
 };
 
+const randMove = (grid, moveLog) => {
+  // Flatten the grid into a single array of moves
+  const allMoves = grid.flatMap((row) => row);
+
+  // Filter out the moves that are already in the moveLog
+  const possibleMoves = allMoves.filter((move) => !moveLog.includes(move));
+
+  // Select a random move from the possible moves
+  const randomMove =
+    possibleMoves[Math.floor(Math.random() * possibleMoves.length)];
+
+  return randomMove;
+};
+
 const Player = (type, gameboard) => {
   const moveLog = [];
 
-  const makeMove = (move) => {
-    // Format the input
-    const fMove = `${move.charAt(0).toUpperCase()}${move.substring(1)}`;
+  const makeMove = (input) => {
+    let move;
+
+    // Check for the type of player
+    if (type === "human") {
+      // Format the input
+      move = `${input.charAt(0).toUpperCase()}${input.substring(1)}`;
+    } else if (type === "computer") {
+      move = randMove(gameboard.grid, moveLog);
+    }
 
     // Check the input against the possible moves on the gameboard's grid
-    if (!checkMove(fMove, gameboard.grid)) {
+    if (!checkMove(move, gameboard.grid)) {
       throw new InvalidMoveEntryError();
     }
 
     // If the move exists in the moveLog array, throw an error
-    if (moveLog.find((el) => el === fMove)) {
+    if (moveLog.find((el) => el === move)) {
       throw new RepeatAttackedError();
     }
 
     // Else, call attack method on gameboard and log move in moveLog
-    gameboard.attack(fMove);
-    moveLog.push(fMove);
+    gameboard.attack(move);
+    moveLog.push(move);
   };
 
   return {
