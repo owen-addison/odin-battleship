5abaf5406efe79b8ba855ef598788d109d282a5f
Owen Lewis <owenalewis@gmail.com>
2023-12-04 14:12:54 +0100

    [test] Creates first test Creates describe block for "Game Setup Tests". Creates the first test for checking the request of input from human type players.

--- __tests__/game.test.js
+++ __tests__/game.test.js
@@ -1,1 +1,6 @@
 import Game from "../src/game";
+
+describe("Game Setup Tests", () => {
+  // Test that the game loop requests ship placement from the human player for the required number of ships
+  test.skip("Game to request ship placement from the human player for the required ships", () => {});
+});
