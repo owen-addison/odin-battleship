149d739dfa956c855bb052314d9e9c5244440a94
Owen Lewis <owenalewis@gmail.com>
2023-12-08 14:41:03 +0100

    [test] Adds test for checking setUp phase of Game The test checks that the Game factory's setUp method correctly places ships on the gameboard for both human and computer players. It does this by mocking the behaviour of the human player's placeShips method.

--- __tests__/game.test.js
+++ __tests__/game.test.js
@@ -14,4 +14,51 @@ describe("Game Initialisation Tests", () => {
     expect(newGame.players.human.gameboard).toBeDefined();
     expect(newGame.players.computer.gameboard).toBeDefined();
   });
+
+  // Test Ship Placement: Verify that ships can be placed correctly on both gameboards during the setup phase.
+  test("Ships to be correctly placed on both gameboards", () => {
+    // Create a new game
+    const game = Game();
+
+    // Mock the human player's ship placement
+    jest.spyOn(game.players.human, "placeShips").mockImplementation(() => {
+      // Simulate placing ships on the human's gameboard
+      game.players.human.gameboard.placeShip("battleship", "D7", "v");
+      game.players.human.gameboard.placeShip("submarine", "A1", "h");
+      game.players.human.gameboard.placeShip("destroyer", "F8", "h");
+      game.players.human.gameboard.placeShip("cruiser", "G1", "h");
+      game.players.human.gameboard.placeShip("carrier", "J6", "v");
+    });
+
+    // Call the setUp method
+    game.setUp();
+
+    // Verify that ships are placed for both players
+    expect(game.players.human.gameboard.ships).toBeDefined();
+    expect(game.players.computer.gameboard.ships).toBeDefined();
+
+    // Assert that ships of all types have been placed
+    const expectedShipTypes = [
+      "battleship",
+      "cruiser",
+      "submarine",
+      "destroyer",
+      "carrier",
+    ];
+    expectedShipTypes.forEach((type) => {
+      expect(game.players.human.gameboard.getShipPositions(type)).toBeDefined();
+      expect(
+        game.players.human.gameboard.getShipPositions(type).length,
+      ).toBeGreaterThan(0);
+      expect(
+        game.players.computer.gameboard.getShipPositions(type),
+      ).toBeDefined();
+      expect(
+        game.players.computer.gameboard.getShipPositions(type).length,
+      ).toBeGreaterThan(0);
+    });
+
+    // Restore the original placeShips method
+    jest.restoreAllMocks();
+  });
 });
