64e066361a3812b7fd779255bffbe05eab7fa503
Owen Lewis <owenalewis@gmail.com>
2023-12-08 13:23:01 +0100

    [test] New test for game initialisation Replaces the old game setup block and unwritten first test with a new test block and the first test. The first test ensures that the game initialises correctly with two players (human and computer) and two gameboards.

--- __tests__/game.test.js
+++ __tests__/game.test.js
@@ -1,6 +1,17 @@
 import Game from "../src/game";
 
-describe("Game Setup Tests", () => {
-  // Test that the game loop requests ship placement from the human player for the required number of ships
-  test.skip("Game to request ship placement from the human player for the required ships", () => {});
+describe("Game Initialisation Tests", () => {
+  // Test Game Setup: Ensure that the game initialises correctly with two players (human and computer) and two gameboards.
+  test("Game to initialise correctly with a human and computer player and their respective gameboards", () => {
+    // Create a new game
+    const newGame = Game();
+
+    // Assert that the game has two players: one human and one computer
+    expect(newGame.players.human).toBeDefined();
+    expect(newGame.players.computer).toBeDefined();
+
+    // Optionally, you can also check if each player has a gameboard
+    expect(newGame.players.human.gameboard).toBeDefined();
+    expect(newGame.players.computer.gameboard).toBeDefined();
+  });
 });
