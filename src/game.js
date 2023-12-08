0a9d3d4dd366280c8dea892d3e9f24c170b92e49
Owen Lewis <owenalewis@gmail.com>
2023-12-08 16:06:40 +0100

    [feature] Adds ship placements functionality Adds ship placement functionality for the setUp phase of the Game factory.

--- src/game.js
+++ src/game.js
@@ -1,6 +1,32 @@
 import Player from "./player";
 import Gameboard from "./gameboard";
 
+const getStartPositionFromUI = () => {};
+
+const getDirectionFromUI = () => {};
+
+const updateUIAfterPlacement = () => {};
+
+const handlePlacementError = () => {};
+
+const triggerHumanShipPlacement = (humanPlayer) => {
+  // Example: Add event listeners to UI elements for ship placement
+  document.querySelectorAll(".ship-placement-button").forEach((button) => {
+    button.addEventListener("click", (event) => {
+      const shipType = event.target.dataset.shipType;
+      const startPosition = getStartPositionFromUI();
+      const direction = getDirectionFromUI();
+
+      try {
+        humanPlayer.gameboard.placeShip(shipType, startPosition, direction);
+        updateUIAfterPlacement();
+      } catch (error) {
+        handlePlacementError(error); // Handle any errors (e.g., overlapping ships)
+      }
+    });
+  });
+};
+
 const Game = () => {
   // Initialise, create gameboards for both players and create players of types human and computer
   const humanGameboard = Gameboard();
@@ -10,6 +36,16 @@ const Game = () => {
 
   // Store players in a player object
   const players = { human: humanPlayer, computer: computerPlayer };
+
+  // Set up phase
+  const setUp = () => {
+    // Automatic placement for computer
+    computerPlayer.placeShips();
+
+    // Trigger or prompt for human player's ship placement
+    // This could be an event or callback that integrates with the UI
+    triggerHumanShipPlacement(humanPlayer);
+  };
 
   return {
     players,
