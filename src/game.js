d1e7e118e52330fa1633ed838ba334ccb4073f25
Owen Lewis <owenalewis@gmail.com>
2023-12-08 13:44:41 +0100

    [feature] Adds initialisation code for Game Adds the initialisation code for the Game factory. When constructor is called, Game creates the defualt setup of creating two gameboards and players, for types "human" and "computer". These players are stored in an object called `players` alongside keys relating to their player type.

--- src/game.js
+++ src/game.js
@@ -1,5 +1,19 @@
 import Player from "./player";
+import Gameboard from "./gameboard";
 
-const Game = () => {};
+const Game = () => {
+  // Initialise, create gameboards for both players and create players of types human and computer
+  const humanGameboard = Gameboard();
+  const computerGameboard = Gameboard();
+  const humanPlayer = Player(humanGameboard, "human");
+  const computerPlayer = Player(computerGameboard, "computer");
+
+  // Store players in a player object
+  const players = { human: humanPlayer, computer: computerPlayer };
+
+  return {
+    players,
+  };
+};
 
 export default Game;
