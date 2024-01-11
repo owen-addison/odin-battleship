8b51a1aa29d02c893611d43c231caacdbb27dbd3
Owen Lewis <owenalewis@gmail.com>
2024-01-11 16:00:10 +0000

    [feature] Adds takeTurn method Adds the takeTurn method and uses a new variable currentPlayer to determine which player to trigger makeMove and which to use as the opponent. Switches the player at the end of turn. Also sets the currentPlayer to the humanPlayer on setUp.

--- src/game.js
+++ src/game.js
@@ -1,6 +1,7 @@
 import Player from "./player";
 import Gameboard from "./gameboard";
 import Ship from "./ship";
+import { InvalidPlayerTypeError } from "./errors";
 
 const Game = () => {
   // Initialise, create gameboards for both players and create players of types human and computer
@@ -8,6 +9,7 @@ const Game = () => {
   const computerGameboard = Gameboard(Ship);
   const humanPlayer = Player(humanGameboard, "human");
   const computerPlayer = Player(computerGameboard, "computer");
+  let currentPlayer;
 
   // Store players in a player object
   const players = { human: humanPlayer, computer: computerPlayer };
@@ -21,11 +23,31 @@ const Game = () => {
     humanShips.forEach((ship) => {
       humanPlayer.placeShips(ship.shipType, ship.start, ship.direction);
     });
+
+    // Set the current player to human player
+    currentPlayer = humanPlayer;
+  };
+
+  // Take turn method
+  const takeTurn = (move) => {
+    // Determine the opponent based on the current player
+    const opponent =
+      currentPlayer === humanPlayer ? computerPlayer : humanPlayer;
+
+    // Call the makeMove method on the current player with the opponent's gameboard
+    currentPlayer.makeMove(opponent.gameboard, move);
+
+    // Switch the current player
+    currentPlayer = opponent;
   };
 
   return {
+    get currentPlayer() {
+      return currentPlayer;
+    },
     players,
     setUp,
+    takeTurn,
   };
 };
 
