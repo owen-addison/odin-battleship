29d16db579ce3a087af1198ae5a8565176c68124
Owen Lewis <owenalewis@gmail.com>
2024-01-16 15:31:50 +0000

    Updates comment for makeMove's return

--- src/player.js
+++ src/player.js
@@ -130,7 +130,7 @@ const Player = (gameboard, type) => {
     // Else, call attack method on gameboard and log move in moveLog
     const response = oppGameboard.attack(move);
     moveLog.push(move);
-    // Return the response of the attack (true for "hit"; false for "miss")
+    // Return the response of the attack (object: { hit: false } for miss; { hit: true, shipType: string } for hit).
     return response;
   };
 
