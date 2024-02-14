1f9790b13767628d6d511bc3f0d0136b2142b462
Owen Lewis <owenalewis@gmail.com>
2024-02-14 15:00:49 +0100

    [test] Adds code to test correct functioning Adds code to check that a game can be instantiated and that the players are correctly created.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-console.log("Hello World!");
+import Game from "./game";
+
+// Instantiate a new game
+const newGame = Game();
+
+// Create a mock array of human player entries
+const humanShips = [
+  { shipType: "battleship", start: "D7", direction: "v" },
+  { shipType: "submarine", start: "A1", direction: "h" },
+  { shipType: "destroyer", start: "F8", direction: "h" },
+  { shipType: "cruiser", start: "G1", direction: "h" },
+  { shipType: "carrier", start: "J6", direction: "v" },
+];
+
+// Call the setUp method on the game
+newGame.setUp(humanShips);
+
+// Console log the players
+console.log(
+  `Players: First player of type ${newGame.players.human.type}, second player of type ${newGame.players.computer.type}!`,
+);
