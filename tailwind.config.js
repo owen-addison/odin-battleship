82edc2adb59ec0f983ae9c63b8e1b76706bf54dd
Owen Lewis <owenalewis@gmail.com>
2024-02-15 16:58:53 +0100

    [setup] Changes index.html path to src over public

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx}", "./public/**/*.html"],
+  content: ["./src/**/*.{js,jsx}", "./src/**/*.html"],
   theme: {
     extend: {},
   },
