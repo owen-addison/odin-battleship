d4e4bc20e68dc21cc93cfc930fc062d0967a1f04
Owen Lewis <owenalewis@gmail.com>
2024-02-15 17:48:18 +0100

    [tooling] Updates path for index.html file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx}", "./src/**/*.html"],
+  content: ["./src/**/*.{js,jsx}", "./dist/**/*.html"],
   theme: {
     extend: {},
   },
