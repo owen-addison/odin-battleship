5d56f1c1d8f05f139591be46e448b3fae423bd26
Owen Lewis <owenalewis@gmail.com>
2024-02-15 17:42:54 +0100

    [setup] Uninstalls HTMLWebpackPlugin Uninstalls dependency and removes corresponding code from webpack.config.js file. This is to try to fix the issue of the script and style tags being loaded twice into the DOM.

--- webpack.config.js
+++ webpack.config.js
@@ -2,19 +2,12 @@ const path = require("path");
 /* eslint-disable import/no-extraneous-dependencies */
 const tailwindcss = require("tailwindcss");
 const autoprefixer = require("autoprefixer");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 /* eslint-enable import/no-extraneous-dependencies */
 
 module.exports = {
   mode: "development",
   entry: "./src/index.js",
   devtool: "inline-source-map",
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: "Development",
-      template: path.resolve(__dirname, "src", "index.html"),
-    }),
-  ],
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
