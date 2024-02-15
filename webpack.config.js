77eba9173ac30a6a2bc78d4e44aebd1f40147c65
Owen Lewis <owenalewis@gmail.com>
2024-02-15 16:14:26 +0100

    [tooling] Adds cleanup rule to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -7,9 +7,11 @@ const autoprefixer = require("autoprefixer");
 module.exports = {
   mode: "development",
   entry: "./src/index.js",
+  devtool: "inline-source-map",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
+    clean: true,
   },
   module: {
     rules: [
