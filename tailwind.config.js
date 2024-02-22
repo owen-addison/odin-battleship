5a51f54d97d65f7464f40863d02d8179a0997ad2
Owen Lewis <owenalewis@gmail.com>
2024-02-22 13:07:23 +0100

    [setup] Tailwind re-intialised

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx}", "./dist/**/*.html"],
+  content: [],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
+
