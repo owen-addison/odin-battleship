2004e363399c21b40c157e61d534e17acf32e7e2
Owen Lewis <owenalewis@gmail.com>
2024-02-26 15:20:30 +0100

    [feature] Expand TailwindCSS content paths
    - Include both /src and /dist directories in TailwindCSS content
      configuration to ensure all dynamic and static class usages are
      captured during the build process.
    - This change addresses potential issues with class purging and
      ensures that TailwindCSS correctly applies styles to dynamically
      generated classes in JavaScript files.
    - Adjustments made to tailwind.config.js to scan for class names
      within JavaScript and HTML files across the entire project,
      improving the reliability of style application and reducing
      the likelihood of encountering caching-related styling issues.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./dist/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js}", "./dist/**/*.{html,js}"],
   theme: {
     extend: {},
   },
