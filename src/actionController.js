f8614718b201ffdfe0849d34e4fb6da3bd274858
Owen Lewis <owenalewis@gmail.com>
2024-02-28 12:13:40 +0100

    [feature] Implement ActionController for UI interactions - Introduced ActionController to manage user interactions. - Integrated gameboardClick function for handling cell clicks,   displaying cell ID, player, and position in the console. - Setup initialisation of console UI and gameboards for both   human and computer within ActionController, leveraging   callbacks for cell clicks. - Added promptShipPlacement method to display ship placement   instructions using UiManager's displayPrompt method.

--- src/actionController.js
+++ src/actionController.js
@@ -1,6 +1,24 @@
 const placeShipPrompt = "Place your ships on the board using the console.";
 
+// Function called when a cell on the gamboard is clicked
+const gameboardClick = (event) => {
+  // Get the target element
+  const { id } = event.target;
+  // Get the target player and position dataset attributes
+  const { player, position } = event.target.dataset;
+  console.log(
+    `Clicked cell ID: ${id}. Player & position: ${player}, ${position}.`,
+  );
+};
+
 const ActionController = (uiManager, game) => {
+  // Initialise console
+  uiManager.initConsoleUI();
+
+  // Initialise gameboard with callback for cell clicks
+  uiManager.createGameboard("human-gb", gameboardClick);
+  uiManager.createGameboard("comp-gb", gameboardClick);
+
   const promptShipPlacement = () => {
     // Create a prompt object with the prompt and prompt type
     const promptObj = { prompt: placeShipPrompt, promptType: "instruction" };
